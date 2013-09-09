2d6425c4de4dc7b4b69c10293c5821243c51fd96
Javier Bali?as Santos <javier@arc-robots.org>
2013-09-09 19:18:45 +0200

    Projects recompiled with updated libraries.

--- software/maindspic/oscillator_config.h
+++ software/maindspic/oscillator_config.h
@@ -1,5 +1,5 @@
 /*  
- *  Copyright JBS (2009)
+ *  Copyright Robotics Association of Coslada, Eurobotics Engineering (2010)
  * 
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -15,15 +15,18 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- *  Revision : $Id:M$
+ *  Revision : $Id$
  *
+ *  Javier Baliñas Santos <javier@arc-robots.org>
  */
 
 
-#ifndef OSCILLATOR_CONFIG_H
-#define OSCILLATOR_CONFIG_H
+#ifndef _OSCILLATOR_CONFIG_H_
+#define _OSCILLATOR_CONFIG_H_
 
+/* ***************************  PIC24F  *****************************/
+/* PLL x4 configuration */
+//#define PLLX4 1
 
 #endif
 
-/* EOF */
