38ff562d02fe1cb5b002cd3b81256ce8db8beeeb
Javier Baliñas Santos <supernudo@users.noreply.github.com>
2013-12-18 12:10:25 +0100

    bug axis_yz

--- software/matlab_api/cnp_commands/cnp_yz_calibrate.m
+++ software/matlab_api/cnp_commands/cnp_yz_calibrate.m
@@ -1,7 +1,7 @@
 function [y_range_mm, z_range_mm] = cnp_yz_calibrate()
 global scnp;
 
-cnp_cmd('axis_yx', 'autopos');
+cnp_cmd('axis_yz', 'autopos');
 y_range_mm = [scnp.y.cmd_min_mm scnp.y.cmd_max_mm];
 z_range_mm = [scnp.z.cmd_min_mm scnp.z.cmd_max_mm];
 
